55f66f293cfcb2fd37e3c36a748e8aaa404c2e3b
Elan Ruusamäe <glen@delfi.ee>
2016-04-20 18:04:34 +0000

    updated from gist to be generic, so it is now tested

--- gh-file-hook.php
+++ gh-file-hook.php
@@ -1,11 +1,9 @@
 <?php
 // https://developer.github.com/webhooks/
-// https://help.github.com/articles/about-webhooks/
-// http://requestb.in/
+// https://gist.github.com/glensc/fb5b55f0d0abc962f3bfb0777ab5546a
 
 // ruby -rsecurerandom -e 'puts SecureRandom.hex(20)'
 define('HOOK_SECRET', '');
-$notify_file = '/home/glen/www/spool/file-sync.json';
 
 set_exception_handler(function($e) {
 	header('HTTP/1.1 500 Internal Server Error');
@@ -15,7 +13,7 @@ set_exception_handler(function($e) {
 
 // https://developer.github.com/v3/activity/events/types/#pushevent
 // X-Hub-Signature: sha1=ffd1a5f14b30eaaaca1e84499c372743302bc47d
-function get_payload() {
+function get_payload($secret) {
 	if (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
 		throw new InvalidArgumentException('No event');
 	}
@@ -27,6 +25,7 @@ function get_payload() {
 	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
 		throw new InvalidArgumentException("HTTP header 'X-Hub-Signature' is missing");
 	}
+
 	if (!extension_loaded('hash')) {
 		throw new InvalidArgumentException("Missing 'hash' extension to check the secret code validity.");
 	}
@@ -39,15 +38,19 @@ function get_payload() {
 	}
 
 	$rawPost = file_get_contents('php://input');
-	if ($hash !== hash_hmac($algo, $rawPost, HOOK_SECRET)) {
+	if ($hash !== hash_hmac($algo, $rawPost, $secret)) {
 		throw new InvalidArgumentException("Hook secret ($algo) does not match");
 	}
 
-	if (!isset($_POST['payload'])) {
-		throw new InvalidArgumentException('No payload');
+	if (isset($_POST['payload'])) {
+		// for urlencoded data
+		$payload = $_POST['payload'];
+	} else {
+		// for application/json
+		$payload = $rawPost;
 	}
 
-	return json_decode($_POST['payload']);
+	return json_decode($payload);
 }
 
-$payload = get_payload();
+$payload = get_payload(HOOK_SECRET);
