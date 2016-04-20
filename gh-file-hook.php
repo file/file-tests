e16b4b22b5fec1b6999241df88299d184c5ff859
Elan Ruusamäe <glen@delfi.ee>
2016-04-20 19:31:33 +0000

    travis invoke on github push hook

--- gh-file-hook.php
+++ gh-file-hook.php
@@ -3,7 +3,14 @@
 // https://gist.github.com/glensc/fb5b55f0d0abc962f3bfb0777ab5546a
 
 // ruby -rsecurerandom -e 'puts SecureRandom.hex(20)'
-define('HOOK_SECRET', '');
+define('GITHUB_HOOK_SECRET', '');
+// obtained with 'travis login --org; travis token --org'
+define('TRAVIS_CI_TOKEN', '');
+
+// repo to trigger build on
+define('TRAVIS_REPO', 'glensc/file-tests');
+// and the branch name
+define('TRAVIS_REPO_BRANCH', 'new-logic');
 
 set_exception_handler(function($e) {
 	header('HTTP/1.1 500 Internal Server Error');
@@ -13,7 +20,7 @@ set_exception_handler(function($e) {
 
 // https://developer.github.com/v3/activity/events/types/#pushevent
 // X-Hub-Signature: sha1=ffd1a5f14b30eaaaca1e84499c372743302bc47d
-function get_payload($secret) {
+function get_github_payload($secret, $assoc = false) {
 	if (!isset($_SERVER['HTTP_X_GITHUB_EVENT'])) {
 		throw new InvalidArgumentException('No event');
 	}
@@ -50,7 +57,56 @@ function get_payload($secret) {
 		$payload = $rawPost;
 	}
 
-	return json_decode($payload);
+	return json_decode($payload, $assoc);
 }
 
-$payload = get_payload(HOOK_SECRET);
+function travis_request($repo, $token, $request) {
+	require_once 'UrlUtil.php';
+	$headers = array(
+	  "Content-Type: application/json",
+	  "Accept: application/json",
+	  "Travis-API-Version: 3",
+	  "Authorization: token $token",
+	);
+	$url = sprintf("https://api.travis-ci.org/repo/%s/requests", urlencode($repo));
+	$body = json_encode(array('request' => $request));
+
+
+	$params = array();
+	$options = array(
+		CURLOPT_HTTPHEADER => $headers,
+		CURLOPT_POSTFIELDS => $body,
+	);
+	return UrlUtil::JsonRequest($url, $params, UrlUtil::HTTP_METHOD_POST, $options);
+}
+
+function travis_trigger_build($payload) {
+	$diff = $payload['compare'];
+	$repo = $payload['repository']['full_name'];
+	$commit = $payload['head_commit'];
+	error_log("PUSH[$repo]: $diff");
+
+	$branch = substr($payload['ref'], 11);
+	$commit_id = substr($commit['id'], 0, 7);
+	$commit_message = $commit['message'];
+	$author = $commit['author']['name'];
+
+	$message = "$author($repo:$commit_id): $commit_message";
+	$request = array(
+		'message' => $message,
+		'branch' => TRAVIS_REPO_BRANCH,
+		'config' => array(
+			'env' => array(
+				'global' => array(
+					"repository={$payload['repository']['clone_url']}",
+					"commit=$commit_id",
+				),
+			),
+		),
+	);
+
+	return travis_request(TRAVIS_REPO, TRAVIS_CI_TOKEN, $request);
+}
+
+$payload = get_github_payload(GITHUB_HOOK_SECRET, 1);
+$res = travis_trigger_build($payload);
