2bb9c65a57dbb1b8e58ce4e2987ad104a844062d
Christian Herdtweck <christian.herdtweck@intra2net.com>
2019-01-14 10:02:46 +0100

    pep8-ify, pylint-ify db.py: invalid var names

--- pyfile/db.py
+++ pyfile/db.py
@@ -39,42 +39,43 @@ def set_stored_metadata(filename, metadata):
         pickle.dump(metadata, file_handle)
 
 
-def is_regression(m1, m2, exact=False, ratio=0.7):
+def is_regression(meta1, meta2, exact=False, ratio=0.7):
     """
     Determine whether two file(1) outputs for same entry are incompatible.
 
     Metadata can be obtained from py:func`get_stored_metadata` or
     :py:func:`file.get_full_metadata`.
 
-    :param dict m1: metadata for entry1.
-    :param dict m2: metadata for entry2.
+    :param dict meta1: metadata for entry1.
+    :param dict meta2: metadata for entry2.
     :param bool exact: whether output has to match letter for letter (True) or
                        whether slight changes are allowed.
     :param float ratio: Amount of difference required for slightly different
                         entries to be considered the same:
                         `0` = all changes allowed; `1` = need perfect match.
-    :returns: True if there is a (significant) difference between `m1` and `m2`
+    :returns: True if there is a (significant) difference between `meta1`
+              and `meta2`.
 
     .. todo:: Reduce code duplication with function get_diff
     """
-    if m1['output'] is None or m2['output'] is None:
+    if meta1['output'] is None or meta2['output'] is None:
         return True
-    if m1['output'] != m2['output']:
+    if meta1['output'] != meta2['output']:
         # previous file didn't detect it, so we hope new output is ok
-        if not m1['output'].endswith("data\n"):
+        if not meta1['output'].endswith("data\n"):
             if exact:
-                if m1['output'] != m2['output']:
+                if meta1['output'] != meta2['output']:
                     return True
             else:
-                match = difflib.SequenceMatcher(None, m1['output'],
-                                                m2['output']).ratio()
+                match = difflib.SequenceMatcher(None, meta1['output'],
+                                                meta2['output']).ratio()
                 if match < ratio:
                     # print >> sys.stderr, "Expected:%sGot     :%s" \
-                    #          % (m2['output'], m1['output'])
+                    #          % (meta2['output'], meta1['output'])
                     return True
 
-    mime = m2['mime'].split(":")[-1].split(";")[0].strip()
-    old_mime = m1['mime'].split(":")[-1].split(";")[0].strip()
+    mime = meta2['mime'].split(":")[-1].split(";")[0].strip()
+    old_mime = meta1['mime'].split(":")[-1].split(";")[0].strip()
 
     # if old_mime is empty, then previous version of File didn't know that
     # filetype.  we will hope that new mime is right.
@@ -92,7 +93,7 @@ def is_regression(m1, m2, exact=False, ratio=0.7):
     return False
 
 
-def get_diff(m1, m2, exact=False, ratio=0.7):
+def get_diff(meta1, meta2, exact=False, ratio=0.7):
     """
     Get textual description about how well file(1) outputs match.
 
@@ -101,26 +102,26 @@ def get_diff(m1, m2, exact=False, ratio=0.7):
 
     .. todo:: Reduce code duplication with function is_regression
     """
-    if m1['output'] is None or m2['output'] is None:
+    if meta1['output'] is None or meta2['output'] is None:
         return "Output is None, was there error during File execution?"
 
     text = ""
-    if m1['output'] != m2['output']:
+    if meta1['output'] != meta2['output']:
         # previous file didn't detect it, so we hope new output is ok
-        if not m1['output'].endswith("data\n"):
+        if not meta1['output'].endswith("data\n"):
             if exact:
-                if m1['output'] != m2['output']:
-                    text = "Expected :%sGot      :%s" % (m1['output'],
-                                                         m2['output'])
+                if meta1['output'] != meta2['output']:
+                    text = "Expected :%sGot      :%s" % (meta1['output'],
+                                                         meta2['output'])
             else:
-                match = difflib.SequenceMatcher(None, m1['output'],
-                                                m2['output']).ratio()
+                match = difflib.SequenceMatcher(None, meta1['output'],
+                                                meta2['output']).ratio()
                 if match < ratio:
-                    text = "Expected :%sGot      :%s" % (m1['output'],
-                                                         m2['output'])
+                    text = "Expected :%sGot      :%s" % (meta1['output'],
+                                                         meta2['output'])
 
-    mime = m2['mime'].split(":")[-1].split(";")[0].strip()
-    old_mime = m1['mime'].split(":")[-1].split(";")[0].strip()
+    mime = meta2['mime'].split(":")[-1].split(";")[0].strip()
+    old_mime = meta1['mime'].split(":")[-1].split(";")[0].strip()
 
     want_mime_diff = False
 
@@ -135,14 +136,14 @@ def get_diff(m1, m2, exact=False, ratio=0.7):
                 want_mime_diff = True
         want_mime_diff = True    # TODO: this invalidates lines above
     if want_mime_diff:
-        text += "Expected :%sGot      :%s" % (m1['mime'], m2['mime'])
+        text += "Expected :%sGot      :%s" % (meta1['mime'], meta2['mime'])
 
     if text != "":
-        if ('pattern' in m1) and ('pattern' in m2) and \
-                m1['pattern'] != "" and m2['pattern'] != "":
-            for line in \
-                    difflib.unified_diff(StringIO(m1['pattern']).readlines(),
-                                         StringIO(m2['pattern']).readlines()):
+        if ('pattern' in meta1) and ('pattern' in meta2) and \
+                meta1['pattern'] != "" and meta2['pattern'] != "":
+            for line in difflib.unified_diff(
+                    StringIO(meta1['pattern']).readlines(),
+                    StringIO(meta2['pattern']).readlines()):
                 text += line
     return text
 
@@ -156,25 +157,25 @@ def get_stored_files(dir_name, subdir=True, *args):
     Matched file names are added to the list.
     If there are no additional arguments, all files found in the directory are
     added to the list.
-    Example usage: fileList = dirEntries(r'H:\TEMP', False, 'txt', 'py')
+    Example usage: file_list = dirEntries(r'H:\TEMP', False, 'txt', 'py')
     Only files with 'txt' and 'py' extensions will be added to the list.
-    Example usage: fileList = dirEntries(r'H:\TEMP', True)
+    Example usage: file_list = dirEntries(r'H:\TEMP', True)
     All files and all the files in subdirectories under H:\TEMP will be added
     to the list.
     """
-    fileList = []
-    for file in os.listdir(dir_name):
-        dirfile = os.path.join(dir_name, file)
+    file_list = []
+    for file_name in os.listdir(dir_name):
+        dirfile = os.path.join(dir_name, file_name)
         if os.path.isfile(dirfile):
             if not args:
                 if not dirfile.endswith("pickle") and \
                         not dirfile.endswith(".source.txt"):
-                    fileList.append(dirfile)
+                    file_list.append(dirfile)
             else:
                 if os.path.splitext(dirfile)[1][1:] in args:
-                    fileList.append(dirfile)
+                    file_list.append(dirfile)
         # recursively access file names in subdirectories
         elif os.path.isdir(dirfile) and subdir:
             # print "Accessing directory:", dirfile
-            fileList.extend(get_stored_files(dirfile, subdir, *args))
-    return fileList
+            file_list.extend(get_stored_files(dirfile, subdir, *args))
+    return file_list
